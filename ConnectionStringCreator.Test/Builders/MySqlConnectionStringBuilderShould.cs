1fe617f154982457b8661bbd616b1439376d8598
luis.marcos <luis.marcos@LUISMARCOS>
2017-06-23 14:36:51 +0200

    MySql builder added. Unit test added

---
+++ ConnectionStringCreator.Test/Builders/MySqlConnectionStringBuilderShould.cs
@@ -0,0 +1,343 @@
+﻿using System;
+using ConnectionStringCreator.Builders;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ConnectionStringCreator.Test.Builders
+{
+    [TestClass]
+    public class MySqlConnectionStringBuilderShould
+    {
+        [TestMethod]
+        public void create_basic_connectionString_with_IntegratedSecurity()
+        {
+            MySqlConnectionString mySqlConnectionStringExpected = new MySqlConnectionString()
+            {
+                Server = "server",
+                Database = "database",
+                IntegratedSecurity = true
+            };
+            MySqlConnectionString mySqlConnectionStringBuilder = new MySqlConnectionStringBuilder("server", "database");
+
+
+            Assert.AreEqual(mySqlConnectionStringExpected.GetConnectionString(), mySqlConnectionStringBuilder.GetConnectionString());
+        }
+
+        [TestMethod]
+        public void create_basic_connectionString_with_User_and_Password()
+        {
+            MySqlConnectionString mySqlConnectionStringExpected = new MySqlConnectionString()
+            {
+                Server = "server",
+                Database = "database",
+                User = "user",
+                Password = "password"
+            };
+            MySqlConnectionString mySqlConnectionStringBuilder = new MySqlConnectionStringBuilder("server", "database", "user", "password");
+
+
+            Assert.AreEqual(mySqlConnectionStringExpected.GetConnectionString(), mySqlConnectionStringBuilder.GetConnectionString());
+        }
+
+        [TestMethod]
+        public void remove_User_and_Password_when_sets_true_IntegratedSecurity()
+        {
+            MySqlConnectionString mySqlConnectionStringExpected = new MySqlConnectionString()
+            {
+                Server = "server",
+                Database = "database",
+                IntegratedSecurity = true
+            };
+            MySqlConnectionString mySqlConnectionStringBuilder =
+                new MySqlConnectionStringBuilder("server", "database", "user", "password").WithIntegratedSecurity(true);
+
+
+            Assert.AreEqual(mySqlConnectionStringExpected.GetConnectionString(), mySqlConnectionStringBuilder.GetConnectionString());
+        }
+
+        [TestMethod]
+        public void no_remove_User_and_Password_when_sets_false_IntegratedSecurity()
+        {
+            MySqlConnectionString mySqlConnectionStringExpected = new MySqlConnectionString()
+            {
+                Server = "server",
+                Database = "database",
+                User = "user",
+                Password = "password"
+            };
+            MySqlConnectionString mySqlConnectionStringBuilder =
+                new MySqlConnectionStringBuilder("server", "database", "user", "password").WithIntegratedSecurity(false);
+
+
+            Assert.AreEqual(mySqlConnectionStringExpected.GetConnectionString(), mySqlConnectionStringBuilder.GetConnectionString());
+        }
+
+        [TestMethod]
+        public void add_User_and_removes_IntegratedSecurity_to_ConnectionString_when_User_contains_text()
+        {
+            MySqlConnectionString mySqlConnectionStringExpected = new MySqlConnectionString()
+            {
+                Server = "server",
+                Database = "database",
+                User = "user"
+            };
+            MySqlConnectionString mySqlConnectionStringBuilder =
+                new MySqlConnectionStringBuilder("server", "database").WithUser("user");
+
+
+            Assert.AreEqual(mySqlConnectionStringExpected.GetConnectionString(), mySqlConnectionStringBuilder.GetConnectionString());
+        }
+
+
+        [TestMethod]
+        public void not_add_User_and_no_removes_IntegratedSecurity_to_ConnectionString_when_User_is_Empty()
+        {
+            MySqlConnectionString mySqlConnectionStringExpected = new MySqlConnectionString()
+            {
+                Server = "server",
+                Database = "database",
+                IntegratedSecurity = true
+            };
+            MySqlConnectionString mySqlConnectionStringBuilder =
+                new MySqlConnectionStringBuilder("server", "database").WithUser(string.Empty);
+
+
+            Assert.AreEqual(mySqlConnectionStringExpected.GetConnectionString(), mySqlConnectionStringBuilder.GetConnectionString());
+        }
+
+        [TestMethod]
+        public void not_add_User_and_no_removes_IntegratedSecurity_to_ConnectionString_when_User_is_Null()
+        {
+            MySqlConnectionString mySqlConnectionStringExpected = new MySqlConnectionString()
+            {
+                Server = "server",
+                Database = "database",
+                IntegratedSecurity = true
+            };
+            MySqlConnectionString mySqlConnectionStringBuilder =
+                new MySqlConnectionStringBuilder("server", "database").WithUser(null);
+
+
+            Assert.AreEqual(mySqlConnectionStringExpected.GetConnectionString(), mySqlConnectionStringBuilder.GetConnectionString());
+        }
+
+
+        [TestMethod]
+        public void add_Password_and_removes_IntegratedSecurity_to_ConnectionString_when_Password_contains_text()
+        {
+            MySqlConnectionString mySqlConnectionStringExpected = new MySqlConnectionString()
+            {
+                Server = "server",
+                Database = "database",
+                Password = "password"
+            };
+            MySqlConnectionString mySqlConnectionStringBuilder =
+                new MySqlConnectionStringBuilder("server", "database").WithPassword("password");
+
+
+            Assert.AreEqual(mySqlConnectionStringExpected.GetConnectionString(), mySqlConnectionStringBuilder.GetConnectionString());
+        }
+
+
+        [TestMethod]
+        public void not_add_Password_and_no_removes_IntegratedSecurity_to_ConnectionString_when_Password_is_Empty()
+        {
+            MySqlConnectionString mySqlConnectionStringExpected = new MySqlConnectionString()
+            {
+                Server = "server",
+                Database = "database",
+                IntegratedSecurity = true
+            };
+            MySqlConnectionString mySqlConnectionStringBuilder =
+                new MySqlConnectionStringBuilder("server", "database").WithPassword(string.Empty);
+
+
+            Assert.AreEqual(mySqlConnectionStringExpected.GetConnectionString(), mySqlConnectionStringBuilder.GetConnectionString());
+        }
+
+        [TestMethod]
+        public void not_add_Password_and_no_removes_IntegratedSecurity_to_ConnectionString_when_Password_is_Null()
+        {
+            MySqlConnectionString mySqlConnectionStringExpected = new MySqlConnectionString()
+            {
+                Server = "server",
+                Database = "database",
+                IntegratedSecurity = true
+            };
+            MySqlConnectionString mySqlConnectionStringBuilder =
+                new MySqlConnectionStringBuilder("server", "database").WithPassword(null);
+
+
+            Assert.AreEqual(mySqlConnectionStringExpected.GetConnectionString(), mySqlConnectionStringBuilder.GetConnectionString());
+        }
+
+        [TestMethod]
+        public void add_Encrypt_to_ConnectionString()
+        {
+            MySqlConnectionString mySqlConnectionStringExpected = new MySqlConnectionString()
+            {
+                Server = "server",
+                Database = "database",
+                IntegratedSecurity = true,
+                Encrypt = true
+            };
+            MySqlConnectionString mySqlConnectionStringBuilder =
+                new MySqlConnectionStringBuilder("server", "database").WithEncrypt(true);
+
+
+            Assert.AreEqual(mySqlConnectionStringExpected.GetConnectionString(), mySqlConnectionStringBuilder.GetConnectionString());
+        }
+
+        [TestMethod]
+        public void add_WithSSLMode_to_ConnectionString()
+        {
+            MySqlConnectionString mySqlConnectionStringExpected = new MySqlConnectionString()
+            {
+                Server = "server",
+                Database = "database",
+                IntegratedSecurity = true,
+                SSLMode = MySqlConnectionString.SSLModes.Preferred
+            };
+            MySqlConnectionString mySqlConnectionStringBuilder =
+                new MySqlConnectionStringBuilder("server", "database").WithSSLMode(MySqlConnectionString.SSLModes.Preferred);
+
+
+            Assert.AreEqual(mySqlConnectionStringExpected.GetConnectionString(), mySqlConnectionStringBuilder.GetConnectionString());
+        }
+
+        [TestMethod]
+        public void add_CertificatePassword_to_ConnectionString()
+        {
+            MySqlConnectionString mySqlConnectionStringExpected = new MySqlConnectionString()
+            {
+                Server = "server",
+                Database = "database",
+                IntegratedSecurity = true,
+                CertificatePassword = "certificate password"
+            };
+            MySqlConnectionString mySqlConnectionStringBuilder =
+                new MySqlConnectionStringBuilder("server", "database").WithCertificatePassword("certificate password");
+
+
+            Assert.AreEqual(mySqlConnectionStringExpected.GetConnectionString(), mySqlConnectionStringBuilder.GetConnectionString());
+        }
+
+        [TestMethod]
+        public void add_AllowBatch_to_ConnectionString()
+        {
+            MySqlConnectionString mySqlConnectionStringExpected = new MySqlConnectionString()
+            {
+                Server = "server",
+                Database = "database",
+                IntegratedSecurity = true,
+                AllowBatch = true
+            };
+            MySqlConnectionString mySqlConnectionStringBuilder =
+                new MySqlConnectionStringBuilder("server", "database").WithAllowBatch(true);
+
+
+            Assert.AreEqual(mySqlConnectionStringExpected.GetConnectionString(), mySqlConnectionStringBuilder.GetConnectionString());
+        }
+
+        [TestMethod]
+        public void add_AllowUserVariables_to_ConnectionString()
+        {
+            MySqlConnectionString mySqlConnectionStringExpected = new MySqlConnectionString()
+            {
+                Server = "server",
+                Database = "database",
+                IntegratedSecurity = true,
+                AllowUserVariables = true
+            };
+            MySqlConnectionString mySqlConnectionStringBuilder =
+                new MySqlConnectionStringBuilder("server", "database").WithAllowUserVariables(true);
+
+
+            Assert.AreEqual(mySqlConnectionStringExpected.GetConnectionString(), mySqlConnectionStringBuilder.GetConnectionString());
+        }
+
+        [TestMethod]
+        public void add_AllowZeroDateTime_to_ConnectionString()
+        {
+            MySqlConnectionString mySqlConnectionStringExpected = new MySqlConnectionString()
+            {
+                Server = "server",
+                Database = "database",
+                IntegratedSecurity = true,
+                AllowZeroDateTime = true
+            };
+            MySqlConnectionString mySqlConnectionStringBuilder =
+                new MySqlConnectionStringBuilder("server", "database").WithAllowZeroDateTime(true);
+
+
+            Assert.AreEqual(mySqlConnectionStringExpected.GetConnectionString(), mySqlConnectionStringBuilder.GetConnectionString());
+        }
+
+        [TestMethod]
+        public void add_ConvertZeroDateTime_to_ConnectionString()
+        {
+            MySqlConnectionString mySqlConnectionStringExpected = new MySqlConnectionString()
+            {
+                Server = "server",
+                Database = "database",
+                IntegratedSecurity = true,
+                ConvertZeroDateTime = true
+            };
+            MySqlConnectionString mySqlConnectionStringBuilder =
+                new MySqlConnectionStringBuilder("server", "database").WithConvertZeroDateTime(true);
+
+
+            Assert.AreEqual(mySqlConnectionStringExpected.GetConnectionString(), mySqlConnectionStringBuilder.GetConnectionString());
+        }
+
+        [TestMethod]
+        public void add_ConnectionLifeTime_to_ConnectionString()
+        {
+            MySqlConnectionString mySqlConnectionStringExpected = new MySqlConnectionString()
+            {
+                Server = "server",
+                Database = "database",
+                IntegratedSecurity = true,
+                ConnectionLifeTime = 2
+            };
+            MySqlConnectionString mySqlConnectionStringBuilder =
+                new MySqlConnectionStringBuilder("server", "database").WithConnectionLifeTime(2);
+
+
+            Assert.AreEqual(mySqlConnectionStringExpected.GetConnectionString(), mySqlConnectionStringBuilder.GetConnectionString());
+        }
+
+        [TestMethod]
+        public void add_DefaultCommandTimeout_to_ConnectionString()
+        {
+            MySqlConnectionString mySqlConnectionStringExpected = new MySqlConnectionString()
+            {
+                Server = "server",
+                Database = "database",
+                IntegratedSecurity = true,
+                DefaultCommandTimeout = 22
+            };
+            MySqlConnectionString mySqlConnectionStringBuilder =
+                new MySqlConnectionStringBuilder("server", "database").WithDefaultCommandTimeout(22);
+
+
+            Assert.AreEqual(mySqlConnectionStringExpected.GetConnectionString(), mySqlConnectionStringBuilder.GetConnectionString());
+        }
+
+        [TestMethod]
+        public void add_ConnectionTimeout_to_ConnectionString()
+        {
+            MySqlConnectionString mySqlConnectionStringExpected = new MySqlConnectionString()
+            {
+                Server = "server",
+                Database = "database",
+                IntegratedSecurity = true,
+                ConnectionTimeout = 222
+            };
+            MySqlConnectionString mySqlConnectionStringBuilder =
+                new MySqlConnectionStringBuilder("server", "database").WithConnectionTimeout(222);
+
+
+            Assert.AreEqual(mySqlConnectionStringExpected.GetConnectionString(), mySqlConnectionStringBuilder.GetConnectionString());
+        }
+    }
+}
---
+++ ConnectionStringCreator/Builders/MySqlConnectionStringBuilder.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConnectionStringCreator.Builders
+{
+    public class MySqlConnectionStringBuilder
+    {
+        private MySqlConnectionString mySqlConnectionString;
+
+        public MySqlConnectionStringBuilder(string server, string database)
+        {
+            mySqlConnectionString = new MySqlConnectionString()
+            {
+                Server = server,
+                Database = database,
+                IntegratedSecurity = true
+            };
+        }
+
+        public MySqlConnectionStringBuilder(string server, string database, string user, string password)
+        {
+            mySqlConnectionString = new MySqlConnectionString()
+            {
+                Server = server,
+                Database = database,
+                User = user,
+                Password = password
+            };
+        }
+
+
+
+        public MySqlConnectionStringBuilder WithIntegratedSecurity(bool integratedSecurity)
+        {
+            mySqlConnectionString.IntegratedSecurity = integratedSecurity;
+            if (mySqlConnectionString.IntegratedSecurity)
+            {
+                mySqlConnectionString.User = string.Empty;
+                mySqlConnectionString.Password = string.Empty;
+            }
+            return this;
+        }
+
+        public MySqlConnectionStringBuilder WithUser(string user)
+        {
+            if (!mySqlConnectionString.IsIgnoredWithValue(c => c.User, user))
+            {
+                mySqlConnectionString.User = user;
+                mySqlConnectionString.IntegratedSecurity = false;
+            }
+
+            return this;
+        }
+
+        public MySqlConnectionStringBuilder WithPassword(string password)
+        {
+            if (!mySqlConnectionString.IsIgnoredWithValue(c => c.Password, password))
+            {
+                mySqlConnectionString.Password = password;
+                mySqlConnectionString.IntegratedSecurity = false;
+            }
+
+            return this;
+        }
+
+
+        public MySqlConnectionStringBuilder WithPort(int? port)
+        {
+            mySqlConnectionString.Port = port;
+            return this;
+        }
+        public MySqlConnectionStringBuilder WithEncrypt(bool? encrypt)
+        {
+            mySqlConnectionString.Encrypt = encrypt;
+            return this;
+        }
+        public MySqlConnectionStringBuilder WithSSLMode(MySqlConnectionString.SSLModes sslMode)
+        {
+            mySqlConnectionString.SSLMode = sslMode;
+            return this;
+        }
+        public MySqlConnectionStringBuilder WithCertificatePassword(string certificatePassword)
+        {
+            mySqlConnectionString.CertificatePassword = certificatePassword;
+            return this;
+        }
+        public MySqlConnectionStringBuilder WithAllowBatch(bool? allowBatch)
+        {
+            mySqlConnectionString.AllowBatch = allowBatch;
+            return this;
+        }
+        public MySqlConnectionStringBuilder WithAllowUserVariables(bool? allowUserVariables)
+        {
+            mySqlConnectionString.AllowUserVariables = allowUserVariables;
+            return this;
+        }
+        public MySqlConnectionStringBuilder WithAllowZeroDateTime(bool? allowZeroDateTime)
+        {
+            mySqlConnectionString.AllowZeroDateTime = allowZeroDateTime;
+            return this;
+        }
+        public MySqlConnectionStringBuilder WithConvertZeroDateTime(bool? convertZeroDateTime)
+        {
+            mySqlConnectionString.ConvertZeroDateTime = convertZeroDateTime;
+            return this;
+        }
+        public MySqlConnectionStringBuilder WithConnectionLifeTime(int? connectionLifeTime)
+        {
+            mySqlConnectionString.ConnectionLifeTime = connectionLifeTime;
+            return this;
+        }
+        public MySqlConnectionStringBuilder WithDefaultCommandTimeout(int? defaultCommandTimeout)
+        {
+            mySqlConnectionString.DefaultCommandTimeout = defaultCommandTimeout;
+            return this;
+        }
+        public MySqlConnectionStringBuilder WithConnectionTimeout(int? connectionTimeout)
+        {
+            mySqlConnectionString.ConnectionTimeout = connectionTimeout;
+            return this;
+        }
+
+        public static implicit operator MySqlConnectionString(MySqlConnectionStringBuilder builder)
+        {
+            return builder.mySqlConnectionString;
+        }
+    }
+}
